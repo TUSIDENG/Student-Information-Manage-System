3b9d844a280cd1492c29f30c16c1dc69cab3f7a1
TUSIDENG <1561799709@qq.com>
2018-11-19 22:54:56 +0800

    对邮箱增加必须，唯一性验证

--- apps/common/validate/Teacher.php
+++ apps/common/validate/Teacher.php
@@ -15,6 +15,6 @@ class Teacher extends Validate
         'username' => 'require|unique:teacher|length:4,25',
         'name' => 'require|length:2,25',
         'sex' => 'in:0,1',
-        'email' => 'email',
+        'email' => 'require|email|unique:teacher',
     ];
 }
